61e120910ba1bda19c41cd217a8449e472b2b4da
Wallace Sardinha <coemeteriu@gmail.com>
2025-03-17 15:00:33 -0300

    update easter-egg

--- main.js
+++ main.js
@@ -67,15 +67,15 @@ form.onsubmit = function (event) {
     } else {
         document.getElementById("height").style.border = "2px solid black";
     }
-/*
-    if (!inputname.value) {
+    /*
+        if (!inputname.value) {
+            
+           document.getElementById("name").style.border = "2px solid red";
+        } else {
+            document.getElementById("name").style.border = "2px solid black";
+        }
         
-       document.getElementById("name").style.border = "2px solid red";
-    } else {
-        document.getElementById("name").style.border = "2px solid black";
-    }
-    
-*/
+    */
     if (inputError) {
         return;
     }
@@ -195,136 +195,159 @@ form.onsubmit = function (event) {
 
         case A1: {
             if (buttonM.checked) {
-            RG.innerText = message0;
-            RG.style.color = "#e51f1f";
-            info.innerText = `A magreza grave (IMC abaixo de 16) é uma condição que pode ser causada por desnutrição severa. Assim, consulte um médico, pois este peso é uma ameaça à saúde.`;
-        }} break;
+                RG.innerText = message0;
+                RG.style.color = "#e51f1f";
+                info.innerText = `A magreza grave (IMC abaixo de 16) é uma condição que pode ser causada por desnutrição severa. Assim, consulte um médico, pois este peso é uma ameaça à saúde.`;
+            }
+        } break;
 
 
 
         case B1: {
             if (buttonM.checked) {
-            RG.innerText = message1;
-            RG.style.color = "#f2a134";
-            info.innerText = 'Condições hormonais, como o hipertireoidismo, podem afetar o peso de uma pessoa, lém de parasitas ou simplesmente uma dieta com poucas calorias diárias. Nesse sentido, você pode consultar um médico para ter certeza de que não tem nada de errado.';
-        }} break;
+                RG.innerText = message1;
+                RG.style.color = "#f2a134";
+                info.innerText = 'Condições hormonais, como o hipertireoidismo, podem afetar o peso de uma pessoa, lém de parasitas ou simplesmente uma dieta com poucas calorias diárias. Nesse sentido, você pode consultar um médico para ter certeza de que não tem nada de errado.';
+            }
+        } break;
 
 
 
         case C1: {
             if (buttonM.checked) {
-            RG.innerText = message2;
-            RG.style.color = "#f7e379";
-            info.innerText = `Pode ter algumas consequências, mas no geral não é preocupante. Logo, 
+                RG.innerText = message2;
+                RG.style.color = "#f7e379";
+                info.innerText = `Pode ter algumas consequências, mas no geral não é preocupante. Logo, 
                 um IMC acima de 17 não fica muito longe do saudável.`;
-        }} break;
+            }
+        } break;
 
 
 
         case D1: {
             if (buttonM.checked) {
-            RG.innerText = message3;
-            RG.style.color = "#44ce1b";
-            info.innerText = `Essa classificação garante um risco menor para diversas doenças que variam de anemia a infarto. 
+                RG.innerText = message3;
+                RG.style.color = "#44ce1b";
+                info.innerText = `Essa classificação garante um risco menor para diversas doenças que variam de anemia a infarto. 
                 Estar nessa classificação é estar no peso ideal para seu corpo, mas é bom lembrar de verificar a circunferência 
                 da cintura em busca de excesso de gordura.`;
-        }} break;
+            }
+        } break;
 
 
 
         case E1: {
             if (buttonM.checked) {
-            RG.innerText = message4;
-            RG.style.color = "#f7e379";
-            info.innerText = `O sobrepeso pode causar alguns problemas de circulação no corpo, além de fadiga. Mas assim como a magreza leve, 
+                RG.innerText = message4;
+                RG.style.color = "#f7e379";
+                info.innerText = `O sobrepeso pode causar alguns problemas de circulação no corpo, além de fadiga. Mas assim como a magreza leve, 
                     se o IMC estiver pouco acima de 25, não é preocupante. Ou seja, uma dieta com um pouco menos de calorias ou um pouco mais de exercícios
                     na rotina pode resolver a situação.`;
-        }} break;
+            }
+        } break;
 
 
 
 
         case F1: {
             if (buttonM.checked) {
-            RG.innerText = message5;
-            RG.style.color = "#f2a134";
-            info.innerText = `Acima do IMC 30 a pessoa é considerada obesa, o que por si só já é uma doença. Graças ao peso, o risco para diversas 
+                RG.innerText = message5;
+                RG.style.color = "#f2a134";
+                info.innerText = `Acima do IMC 30 a pessoa é considerada obesa, o que por si só já é uma doença. Graças ao peso, o risco para diversas 
                     condições aumenta consideravelmente. Um médico pode indicar um meio ideal para a regulação do peso.`;
-        }} break;
+            }
+        } break;
 
 
         case G1: {
             if (buttonM.checked) {
-            RG.innerText = message6;
-            RG.style.color = "#e51f1f";
-            info.innerText = `Estar com obesidade grau II é ter riscos elevados de diabetes, hipertensão, além de câncer e infarto. Procure um médico 
+                RG.innerText = message6;
+                RG.style.color = "#e51f1f";
+                info.innerText = `Estar com obesidade grau II é ter riscos elevados de diabetes, hipertensão, além de câncer e infarto. Procure um médico 
                     para lidar com a situação.`;
-        }} break;
+            }
+        } break;
 
 
 
 
         case H1: {
             if (buttonM.checked) {
-            RG.innerText = message7;
-            RG.style.color = "#e51f1f";
-            info.innerText = `Chamada de obesidade mórbida, esta condição representa sérios riscos à saúde. Procure ajuda médica.`;
-        }} break;
+                RG.innerText = message7;
+                RG.style.color = "#e51f1f";
+                info.innerText = `Chamada de obesidade mórbida, esta condição representa sérios riscos à saúde. Procure ajuda médica.`;
+            }
+        } break;
     }
 
-    
-    
+
+
     const messageWelcomeM = `Bem vindo, 
     ${name}!`
     const messageWelcomeF = `Bem vinda, 
     ${name}!`
-            const messageWelcomeEM = `Bem vindo!`
-            const messageWelcomeEF = `Bem vinda!`
-            document.querySelector('.modal-wrapper .h3 span').innerText = messageInfo;
-            const message = `seu IMC é de ${result}.`
-            modalMessage.innerText = message
-            
-            if (buttonM.checked) {
-                modalName.innerText = messageWelcomeM
-            } else {
-                modalName.innerText = messageWelcomeF
-            } if (!name && buttonM.checked) {
-                modalName.innerText = messageWelcomeEM
-            } else if (!name && buttonF.checked) {
-                modalName.innerText = messageWelcomeEF
-            }
+    const messageWelcomeEM = `Bem vindo!`
+    const messageWelcomeEF = `Bem vinda!`
+    document.querySelector('.modal-wrapper .h3 span').innerText = messageInfo;
+    const message = `seu IMC é de ${result}.`
+    modalMessage.innerText = message
 
-
-const lowerName = inputname.value.toLowerCase();
-const specialNames = [
-  "bolinha de ouro",
-  "wan",
-  "wania mara",
-  "wania mara albino",
-  "ludovick",
-  "wan ludovick",
-  "wan lucida",
-  "wan ludovick"
-];
-
-if (specialNames.includes(lowerName)) {
-  modalName.innerText = "Amor da minha vida!"
-                modalMessage.innerText = "Você esta perfeita, como sempre vai estar."
-                info.innerText = "Perfeição e quando você e o grande amor da vida de alguem, nesse caso, nada mais importa, apenas você."
-                RG.innerText = "Perfeição!"
-                RG.style.color = "green"
-                alertMessage.innerText = "Valido somente para meu grande amor."
-            }
-
-            modalWrapper.classList.add('open')
-            
-            
-            const BtnClose = document.getElementById('close')
-            BtnClose.onclick = function (event) {
-                event.target == modalWrapper
-                modalWrapper.style.visibility = "hidden"
-                location.reload()
-                return;
-
-            }
+    if (buttonM.checked) {
+        modalName.innerText = messageWelcomeM
+    } else {
+        modalName.innerText = messageWelcomeF
+    } if (!name && buttonM.checked) {
+        modalName.innerText = messageWelcomeEM
+    } else if (!name && buttonF.checked) {
+        modalName.innerText = messageWelcomeEF
     }
+
+
+    const lowerName = inputname.value.toLowerCase();
+    const specialNames = [
+        "bolinha de ouro",
+        "wan",
+        "wania mara",
+        "wania mara albino",
+        "ludovick",
+        "wan ludovick",
+        "wan lucida",
+        "wan ludovick",
+        "wania"
+    ];
+
+
+
+    if (specialNames.includes(lowerName)) {
+        modalName.innerText = "Amor da minha vida!"
+        modalMessage.innerText = "Você esta perfeita, como sempre vai estar."
+        info.innerText = "Perfeição e quando você e o grande amor da vida de alguem, nesse caso, nada mais importa, apenas você."
+        RG.innerText = "Perfeição!"
+        RG.style.color = "green"
+        alertMessage.innerText = "Valido somente para meu grande amor."
+    }
+
+    modalWrapper.classList.add('open');
+
+    const BtnClose = document.getElementById('close');
+    BtnClose.onclick = function() {
+
+        modalWrapper.classList.remove('open');
+        modalWrapper.style.visibility = "hidden";
+        
+
+        form.reset();
+        
+
+        document.getElementById("weight").style.border = "2px solid black";
+        document.getElementById("height").style.border = "2px solid black";
+        
+
+        RG.innerText = "";
+        info.innerText = "";
+        modalMessage.innerText = "";
+        modalName.innerText = "";
+
+        return
+    }
+}
